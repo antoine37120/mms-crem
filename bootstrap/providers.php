a64b5ab7aab2a7ca3215dc7d0dce7425c6e66736
antoine37120 <ant.dupre@gmail.com>
2025-09-25 17:55:16 +0200

    init

---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\MmsAdminPanelProvider::class,
+    App\Providers\FortifyServiceProvider::class,
+    App\Providers\VoltServiceProvider::class,
+];
