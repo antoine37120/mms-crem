e162199d7e45c3002d935c6c4966178c5f79293f
antoine37120 <ant.dupre@gmail.com>
2025-12-18 10:33:05 +0100

    disable default widgets

--- app/Providers/Filament/MmsAdminPanelProvider.php
+++ app/Providers/Filament/MmsAdminPanelProvider.php
@@ -46,8 +46,8 @@ class MmsAdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
-                AccountWidget::class,
-                FilamentInfoWidget::class,
+                /*AccountWidget::class,
+                FilamentInfoWidget::class,*/
             ])
             ->middleware([
                 EncryptCookies::class,
